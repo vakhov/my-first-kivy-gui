759bb67762b86523385c63a5af7fc4665aa24bb3
Alex Vakhov <alex.vakhov@gmail.com>
2019-03-21 23:22:57 +0500

    add Button

--- gui.py
+++ gui.py
@@ -1,10 +1,11 @@
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.button import Button
 
 
 class MyFirstProgramApp(App):
     def build(self):
-        return Label(text='Hello world')
+        return Button(text='Hello world')
 
 
 def main():
