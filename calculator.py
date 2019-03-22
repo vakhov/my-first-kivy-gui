587be0f5b9257df08e9d349ccdfd4ec3c7ff1288
Alex Vakhov <alex.vakhov@gmail.com>
2019-03-22 12:05:40 +0500

    [add] Окно ввода. Кнопки клавиатуры.

--- calculator.py
+++ calculator.py
@@ -9,10 +9,34 @@ from kivy.config import Config
 Config.set('graphics', 'resizable', '0')
 Config.set('graphics', 'width', '300')
 Config.set('graphics', 'height', '300')
+Config.set('kivy', 'exit_on_escape', True)
 
 
 class CalculatorApp(App):
-    pass
+    result: TextInput
+    symbol_buttons = [
+        '7', '8', '9', '+', '<',
+        '4', '5', '6', '-', '(',
+        '1', '2', '3', '*', ')',
+        '0', '.', '=', '/', '%'
+    ]
+
+    def calculator(self, *args, **kwargs):
+        pass
+
+    def build(self):
+        root = BoxLayout(orientation='vertical', padding=5)
+        self.result = TextInput(
+            text='', readonly=True, font_size=25,
+            size_hint=[1, 0.75], background_color=[1, 1, 1, 0.8]
+        )
+        root.add_widget(self.result)
+
+        all_button = GridLayout(cols=5)
+        for symbol in self.symbol_buttons:
+            all_button.add_widget(Button(text=symbol, on_press=self.calculator))
+        root.add_widget(all_button)
+        return root
 
 
 if __name__ == '__main__':
